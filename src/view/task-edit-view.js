67b75a488974278a6f9485bc30e93b89935a08cf
stasrndn <stas.rndn@yandex.ru>
2022-08-29 19:03:23 +0300

    2.10 Добавит логику отображения выбора цвета
    - Вынесем часть шаблона с выбором цвета в отдельную функцию, чтобы не путаться в коде
    - Создадим модуль с константами
    - Опишем условия отображения контрола выбора цвета

--- src/view/task-edit-view.js
+++ src/view/task-edit-view.js
@@ -1,21 +1,21 @@
 import {createElement} from '../render.js';
 import {humanizeTaskDueDate, isTaskRepeating} from "../utils.js";
+import {COLORS} from '../const.js';
 
-const createTaskEditDateTemplate = (dueDate) => (
-  `<button class="card__date-deadline-toggle" type="button">
-      date: <span class="card__date-status">${dueDate !== null ? 'yes' : 'no'}</span>
-    </button>
+const createTaskEditDateTemplate = (dueDate) => (`
+  <button class="card__date-deadline-toggle" type="button">
+    date: <span class="card__date-status">${dueDate !== null ? 'yes' : 'no'}</span>
+  </button>
 
-    ${dueDate !== null ? `<fieldset class="card__date-deadline">
-      <label class="card__input-deadline-wrap">
-        <input class="card__date" type="text" placeholder="" name="date" value="${humanizeTaskDueDate(dueDate)}">
-      </label>
-    </fieldset>` : ''}
-  `
-);
+  ${dueDate !== null ? `<fieldset class="card__date-deadline">
+    <label class="card__input-deadline-wrap">
+      <input class="card__date" type="text" placeholder="" name="date" value="${humanizeTaskDueDate(dueDate)}">
+    </label>
+  </fieldset>` : ''}
+`);
 
-const createTaskEditRepeatingTemplate = (repeating) => (
-  `<button class="card__repeat-toggle" type="button">
+const createTaskEditRepeatingTemplate = (repeating) => (`
+  <button class="card__repeat-toggle" type="button">
     repeat:<span class="card__repeat-status">${isTaskRepeating(repeating) ? 'yes' : 'no'}</span>
   </button>
 
@@ -34,32 +34,35 @@ const createTaskEditRepeatingTemplate = (repeating) => (
           <label class="card__repeat-day" for="repeat-${day}">${day}</label>
     `).join('')}
         </div>
-    </fieldset>` : ''}`
-);
+    </fieldset>` : ''}`);
+
+const createTaskEditColorsTemplate = (currentColor) => COLORS.map((color) => `
+  <input
+    type="radio"
+    id="color-${color}"
+    class="card__color-input card__color-input--${color} visually-hidden"
+    name="color"
+    value="${color}"
+    ${currentColor === color ? 'checked' : ''}
+    />
+  <label
+    for="color-${color}"
+    class="card__color card__color--${color}"
+    >${color}</label>
+`
+).join('');
 
 const createTaskEditTemplate = (task = {}) => {
   const {
-    color = 'black',
-    description = '',
-    dueDate = null,
-    repeating = {
-      mo: false,
-      tu: false,
-      we: false,
-      th: false,
-      fr: false,
-      sa: false,
-      su: false,
+    color = 'black', description = '', dueDate = null, repeating = {
+      mo: false, tu: false, we: false, th: false, fr: false, sa: false, su: false,
     },
   } = task;
 
   const dateTemplate = createTaskEditDateTemplate(dueDate);
-
   const repeatingTemplate = createTaskEditRepeatingTemplate(repeating);
-
-  const repeatingClassName = isTaskRepeating(repeating)
-    ? 'card--repeat'
-    : '';
+  const repeatingClassName = isTaskRepeating(repeating) ? 'card--repeat' : '';
+  const colorsTemplate = createTaskEditColorsTemplate(color);
 
   return (`<article class="card card--edit card--${color} ${repeatingClassName}">
       <form class="card__form" method="get">
@@ -79,7 +82,6 @@ const createTaskEditTemplate = (task = {}) => {
           <div class="card__settings">
             <div class="card__details">
               <div class="card__dates">
-
                 ${dateTemplate}
                 ${repeatingTemplate}
               </div>
@@ -88,16 +90,7 @@ const createTaskEditTemplate = (task = {}) => {
             <div class="card__colors-inner">
               <h3 class="card__colors-title">Color</h3>
               <div class="card__colors-wrap">
-                <input type="radio" id="color-black-4" class="card__color-input card__color-input--black visually-hidden" name="color" value="black">
-                <label for="color-black-4" class="card__color card__color--black">black</label>
-                <input type="radio" id="color-yellow-4" class="card__color-input card__color-input--yellow visually-hidden" name="color" value="yellow" checked="">
-                <label for="color-yellow-4" class="card__color card__color--yellow">yellow</label>
-                <input type="radio" id="color-blue-4" class="card__color-input card__color-input--blue visually-hidden" name="color" value="blue">
-                <label for="color-blue-4" class="card__color card__color--blue">blue</label>
-                <input type="radio" id="color-green-4" class="card__color-input card__color-input--green visually-hidden" name="color" value="green">
-                <label for="color-green-4" class="card__color card__color--green">green</label>
-                <input type="radio" id="color-pink-4" class="card__color-input card__color-input--pink visually-hidden" name="color" value="pink">
-                <label for="color-pink-4" class="card__color card__color--pink">pink</label>
+                ${colorsTemplate}
               </div>
             </div>
           </div>
