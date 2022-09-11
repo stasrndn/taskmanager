2f13e729833c987be815993df4bc8b0dbb567404
stasrndn <stas.rndn@yandex.ru>
2022-09-11 18:03:35 +0300

    4.1.3 Добавит наследование простым компонентам
    Простые - компоненты, не требующие конструктора

--- src/view/new-task-button-view.js
+++ src/view/new-task-button-view.js
@@ -1,23 +1,11 @@
-import {createElement} from '../render.js';
+import AbstractView from '../framework/view/abstract-view.js';
 
 const createNewTaskButtonTemplate = () => '<button class="control__button">+ ADD NEW TASK</button>';
 
-export default class NewTaskButtonView {
-  #element = null;
+export default class NewTaskButtonView extends AbstractView {
 
   get template() {
     return createNewTaskButtonTemplate();
   }
 
-  get element() {
-    if (!this.#element) {
-      this.#element = createElement(this.template);
-    }
-
-    return this.#element;
-  }
-
-  removeElement() {
-    this.#element = null;
-  }
 }
