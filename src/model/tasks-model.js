ec8ce886fa2ada88734fd0b067da4cc128813cbf
stasrndn <stas.rndn@yandex.ru>
2022-09-25 19:34:33 +0300

    7.3 Доработает модель и датабиндинг. WIP
    - Опишем в модели методы добавления, обновления и удаления задачи
    - #handleTaskChange заменим на обработчик любого пользовательского действия (пока пустой)
    - добавим обработчик-наблюдатель #handleModelEvent, который будет реагировать на изменения модели
    
    WIP: линтер ругается на консоль. По-прежнему не работает обновление

--- src/model/tasks-model.js
+++ src/model/tasks-model.js
@@ -1,13 +1,50 @@
-import Observable from "../framework/observable";
+import Observable from '../framework/observable.js';
 import {generateTask} from '../mock/task.js';
 
-/**
- * Модель рыба для списка задач
- */
 export default class TasksModel extends Observable {
   #tasks = Array.from({length: 22}, generateTask);
 
   get tasks() {
     return this.#tasks;
   }
+
+  updateTask = (updateType, update) => {
+    const index = this.#tasks.findIndex((task) => task.id === update.id);
+
+    if (index === -1) {
+      throw new Error('Can\'t update unexisting task');
+    }
+
+    this.#tasks = [
+      ...this.#tasks.slice(0, index),
+      update,
+      ...this.#tasks.slice(index + 1),
+    ];
+
+    this._notify(updateType, update);
+  };
+
+  addTask = (updateType, update) => {
+    this.#tasks = [
+      update,
+      ...this.#tasks,
+    ];
+
+    this._notify(updateType, update);
+  };
+
+  deleteTask = (updateType, update) => {
+    const index = this.#tasks.findIndex((task) => task.id === update.id);
+
+    if (index === -1) {
+      throw new Error('Can\'t delete unexisting task');
+    }
+
+    this.#tasks = [
+      ...this.#tasks.slice(0, index),
+      ...this.#tasks.slice(index + 1),
+    ];
+
+    this._notify(updateType);
+  };
 }
