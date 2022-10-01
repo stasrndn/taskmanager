7a78d402d64eb9cd234157390ceaba15b21057e1
stasrndn <stas.rndn@yandex.ru>
2022-10-01 14:26:16 +0300

    8.1.3 Реализует паттерн "Адаптер". WIP
    - Добавим в модель метод для переделки структуры данных сервера в структуру данных клиента
    - Добавим в модуль TasksApiService метод для переделки структуры данных клиента в структуру данных сервера
    - Используем эти методы, чтобы переводить в серверную структуру данные перед обновлением задачи и в клиентскую структуру при получении списка задач
    
    WIP: линтер ругается на консоль

--- src/tasks-api-service.js
+++ src/tasks-api-service.js
@@ -1,4 +1,4 @@
-import ApiService from "./framework/api-service.js";
+import ApiService from './framework/api-service.js';
 
 const Method = {
   GET: 'GET',
@@ -6,29 +6,38 @@ const Method = {
 };
 
 export default class TasksApiService extends ApiService {
-  /**
-   * Метод для получения задач
-   * @returns {Promise<Response>}
-   */
   get tasks() {
-    return this
-      ._load({url: 'tasks'})
-      .then(ApiService.parseResponse)
+    return this._load({url: 'tasks'})
+      .then(ApiService.parseResponse);
   }
 
-  /**
-   * Метод для обновления задач
-   * @param task
-   * @returns {Promise<JSON>}
-   */
   updateTask = async (task) => {
     const response = await this._load({
       url: `tasks/${task.id}`,
       method: Method.PUT,
-      body: JSON.stringify(task),
+      body: JSON.stringify(this.#adaptToServer(task)),
       headers: new Headers({'Content-Type': 'application/json'}),
     });
 
-    return await ApiService.parseResponse(response);
+    const parsedResponse = await ApiService.parseResponse(response);
+
+    return parsedResponse;
+  };
+
+  #adaptToServer = (task) => {
+    const adaptedTask = {...task,
+      'due_date': task.dueDate instanceof Date ? task.dueDate.toISOString() : null, // На сервере дата хранится в ISO формате
+      'is_archived': task.isArchive,
+      'is_favorite': task.isFavorite,
+      'repeating_days': task.repeating,
+    };
+
+    // Ненужные ключи мы удаляем
+    delete adaptedTask.dueDate;
+    delete adaptedTask.isArchive;
+    delete adaptedTask.isFavorite;
+    delete adaptedTask.repeating;
+
+    return adaptedTask;
   };
 }
